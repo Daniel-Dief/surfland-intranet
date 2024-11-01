fbb6ebcc77de2d8d1a7a710f130575475157c6c0
Daniel Santos <danieldiefsantos@gmail.com>
2024-11-01 15:02:15 -0300

    feat: creating Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { Container, List } from './styles'
+import NavButton from '../NavButton'
+
+export default function Header() {
+    return (
+        <Container>
+            <List>
+                <li>
+                    <NavButton
+                        text="Inicio"
+                        imgPath={require("../../assets/images/logo.png")}
+                        hrefText="Home"
+                    />
+                </li>
+                <li>
+                    <NavButton
+                        text="Sessões"
+                        imgPath={require("../../assets/images/ticket.png")}
+                        hrefText="Sessions"
+                    />
+                </li>
+                <li>
+                    <NavButton
+                        text="Usuário"
+                        imgPath={require("../../assets/images/user.png")}
+                    />
+                </li>
+            </List>
+        </Container>
+    )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3.5rem;
+    background-color: red;
+`;
+
+export const List = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0;
+    margin: 2rem;
+    list-style: none;
+`
