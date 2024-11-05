df247d6187c674ac024bf85de23fa745107f9cac
Daniel Santos <danieldiefsantos@gmail.com>
2024-11-05 14:54:29 -0300

    feat: adding containers to home page

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,7 +1,25 @@
 import Header from '../../components/Header';
+import Container from '../../components/Container';
+import { Main } from './styles';
 
 export default function Home() {
     return(
-        <Header />
+        <>
+            <Header />
+            <Main>
+                <Container
+                    title="Minhas Sessões"
+                    imgPath={require("../../assets/images/sunshade.png")}
+                    linkHref="MySessions"
+                    linkText="Minhas sessões"
+                />
+                <Container
+                    title="Bora surfar!"
+                    imgPath={require("../../assets/images/surf.png")}
+                    linkHref="Schedule"
+                    linkText="Agendar"
+                />
+            </Main>
+        </>
     )
 }
