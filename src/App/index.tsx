f124b13cce4310825bf7506a45d18a5cb0f7cfd2
Daniel Santos <danieldiefsantos@gmail.com>
2024-11-08 10:58:50 -0300

    feat: adding route to "MySessions"

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ModalCase from '../components/ModalCase';
 
 import Home from "../templates/Home";
+import MySessions from "../templates/MySessions";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="*" element={<Home />} />
+        <Route path="MySessions" element={<MySessions />} />
       </Routes>
       <ModalCase />
     </Router>
