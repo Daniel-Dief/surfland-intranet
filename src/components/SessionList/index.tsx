f95bebef5fc0a267792c9354c1f0fc1e973b4e22
Daniel Santos <danieldiefsantos@gmail.com>
2024-11-18 11:37:51 -0300

    refact: ajusting SessionList to new type and hook

--- src/components/SessionList/index.tsx
+++ src/components/SessionList/index.tsx
@@ -1,96 +1,21 @@
+import { useState } from 'react';
+
 import { SmallIcon } from '../../common/styles/images';
 import { Container, TableBox, Table, Thead, Tbody, Tr, Td, ActionTd, ActionButton, Title, Text, Navbox, NavButton } from './style';
 
+import { useMySessions } from '../../hooks/useMySessions';
+
 export default function SessionList() {
-    const arrJson = [
-        {
-            id: 1,
-            waveType: 'Tipo 1',
-            date: '2021-10-10',
-            time: '09:00',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 2,
-            waveType: 'Tipo 2',
-            date: '2021-10-10',
-            time: '10:30',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 3,
-            waveType: 'Tipo 3',
-            date: '2021-10-10',
-            time: '12:00',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 4,
-            waveType: 'Tipo 4',
-            date: '2021-10-10',
-            time: '13:30',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 5,
-            waveType: 'Tipo 5',
-            date: '2021-10-10',
-            time: '15:00',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 6,
-            waveType: 'Tipo 6',
-            date: '2021-10-10',
-            time: '16:30',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 7,
-            waveType: 'Tipo 7',
-            date: '2021-10-10',
-            time: '18:00',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 8,
-            waveType: 'Tipo 8',
-            date: '2021-10-10',
-            time: '19:30',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 9,
-            waveType: 'Tipo 9',
-            date: '2021-10-10',
-            time: '21:00',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        },
-        {
-            id: 10,
-            waveType: 'Tipo 10',
-            date: '2021-10-10',
-            time: '22:30',
-            createdAt: '2021-10-10 10:00',
-            status: 'Ativo'
-        }
-    ]
+    const [page, setPage] = useState<number>(1);
+       
+    const responseJson = useMySessions(page);
 
     function handlePrevius() {
-        console.log('Previous')
+        if (page > 1) { setPage(page - 1) };
     }
 
     function handleNext() {
-        console.log('Next')
+        if (responseJson.totalPages > page) { setPage(page + 1) };
     }
 
     function handleDelete(id: number) {
@@ -114,9 +39,9 @@ export default function SessionList() {
                     </Thead>
                     <Tbody>
                         {
-                            arrJson.map((item) => (
+                            responseJson.data.map((item) => (
                                 <Tr 
-                                    key={arrJson.indexOf(item)}
+                                    key={responseJson.data.indexOf(item)}
                                 >
                                     <Td><Text>
                                         {item.id}
@@ -125,16 +50,16 @@ export default function SessionList() {
                                         {item.waveType}
                                     </Text></Td>
                                     <Td><Text>
-                                        {item.date}
+                                        {item.waveDate}
                                     </Text></Td>
                                     <Td><Text>
-                                        {item.time}
+                                        {item.waveTime}
                                     </Text></Td>
                                     <Td><Text>
-                                        {item.createdAt}
+                                        {item.createdAt.toISOString()}
                                     </Text></Td>
                                     <Td><Text>
-                                        {item.status}
+                                        {item.statusId}
                                     </Text></Td>
                                     <ActionTd>
                                         <ActionButton
